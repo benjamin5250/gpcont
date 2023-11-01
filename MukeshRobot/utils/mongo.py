16097cf3bae1aefabd6ebb27e061a5f91e334be1
benjamin5250 <146529364+benjamin5250@users.noreply.github.com>
2023-11-01 22:40:24 +0630

    Update mongo.py

--- MukeshRobot/utils/mongo.py
+++ MukeshRobot/utils/mongo.py
@@ -9,7 +9,7 @@ db = mongo.MukeshRobot
 
 coupledb = db.couple
 karmadb = db.karma
-
+matadb = db.mata
 
 async def _get_lovers(chat_id: int):
     lovers = await coupledb.find_one({"chat_id": chat_id})
@@ -120,3 +120,41 @@ async def alpha_to_int(user_id_alphabet: str) -> int:
         user_id += str(index)
     user_id = int(user_id)
     return user_id
+
+#SangMata
+async def cek_userdata(user_id: int) -> bool:
+    user = await matadb.find_one({"user_id": user_id})
+    return bool(user)
+
+
+async def get_userdata(user_id: int) -> bool:
+    user = await matadb.find_one({"user_id": user_id})
+    return user["username"], user["first_name"], user["last_name"]
+
+
+async def add_userdata(user_id: int, username, first_name, last_name):
+    await matadb.update_one(
+        {"user_id": user_id},
+        {
+            "$set": {
+                "username": username,
+                "first_name": first_name,
+                "last_name": last_name,
+            }
+        },
+        upsert=True,
+    )
+
+
+# Enable Mata MissKaty in Selected Chat
+async def is_sangmata_on(chat_id: int) -> bool:
+    chat = await matadb.find_one({"chat_id_toggle": chat_id})
+    return bool(chat)
+
+
+async def sangmata_on(chat_id: int) -> bool:
+    await matadb.insert_one({"chat_id_toggle": chat_id})
+
+
+async def sangmata_off(chat_id: int):
+    await matadb.delete_one({"chat_id_toggle": chat_id})
