42c817f6579177deffe9131e506551ea80a975fc
benjamin5250 <146529364+benjamin5250@users.noreply.github.com>
2023-11-02 21:56:26 +0630

    Update mention.py

--- MukeshRobot/modules/mention.py
+++ MukeshRobot/modules/mention.py
@@ -1,6 +1,9 @@
 from MukeshRobot import pbot as Client # This is bot's Client
 from pyrogram import filters # pyrogram filters
-import os, logging, asyncio
+import asyncio
+
+from telethon import events
+from telethon.errors import UserNotParticipantError
 from telethon import Button
 from telethon import TelegramClient, events
 from telethon.tl.types import ChannelParticipantAdmin
@@ -8,9 +11,7 @@ from telethon.tl.types import ChannelParticipantCreator
 from telethon.tl.types import ChannelParticipantsAdmins
 from telethon.tl.functions.channels import GetParticipantRequest
 from telethon.errors import UserNotParticipantError
-from pyrogram import Client, filters
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
-
+from MukeshRobot import telethn as client
 
 #ғᴏʀ /help ᴍᴇɴᴜ
 __mod_name__ = "MentionAll"
@@ -18,7 +19,66 @@ __help__ = "Module help message"
 
 spam_chats = []
 
-@Client.on(events.NewMessage(pattern="^/mentionall|@mentionall|@all ?(.*)"))
+@client.on(events.NewMessage(pattern="^/start$"))
+async def start(event):
+    chat_id = event.chat_id
+    if not event.is_private:
+        return await event.respond("I'm not dead 😎")
+    await event.reply(
+        "━━━━━━━━━━━━━━━━━━━━━━━━\n\n✪  𝐈 𝐚𝐦 ဂရပ်ထမယ် 𝐌𝐞𝐧𝐭𝐢𝐨𝐧 𝐛𝐨𝐭 𝐭𝐨 𝐦𝐞𝐧𝐭𝐢𝐨𝐧 𝐚𝐥𝐥 𝐭𝐡𝐞 𝐠𝐫𝐨𝐮𝐩 𝐦𝐞𝐦𝐛𝐞𝐫𝐬 𝐢𝐧 𝐭𝐞𝐥𝐞𝐠𝐫𝐚𝐦\n✪ 𝐓𝐡𝐚𝐧𝐤𝐬 𝐟𝐨𝐫 𝐮𝐬𝐢𝐧𝐠 𝐦𝐞 𝐚𝐧𝐝 𝐭𝐲𝐩𝐞 /help..\n\n┏━━━━━━━━━━━━━━━━━┓\n┣★ Owner    : [KAREN](https://t.me/KoShine_Apaing)\n┣★ Group › : [ဂရပ်ထမယ်](https://t.me/EwhWTBjxYuYxYTFl)┓\n┗━━━━━━━━━━━━━━━━━┛\n\n💞 ɪғ ʏᴏᴜ ʜᴀᴠᴇ ᴀɴʏ ǫᴜᴇsᴛɪᴏɴs ᴛʜᴇɴ\nᴅᴍ ᴛᴏ ᴍʏ [ᴏᴡɴᴇʀ](https://t.me/KoShine_Apaing) ...\n\n━━━━━━━━━━━━━━━━━━━━━━━━",
+        link_preview=False,
+        buttons=(
+            [
+                Button.url(
+                    "☀︎︎️ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ☀︎︎",
+                    "https://t.me/Gayetbot?startgroup=true",
+                ),
+            ],
+            [
+                Button.url("☀︎︎ Group ☀︎︎︎", "https://t.me/EwhWTBjxYuYxYTFl"),
+                Button.url("☀︎︎ Owner ☀︎︎", "https://t.me/KoShine_Apaing"),
+            ],
+        ),
+    )
+
+
+@client.on(events.NewMessage(pattern="^/help$"))
+async def help(event):
+    chat_id = event.chat_id
+    if not event.is_private:
+        return await event.respond("ᴅᴇᴀʀ sᴛᴀʀᴛ ᴍᴇ ɪɴ ᴘᴍ ᴛᴏ ᴜsᴇ ᴛʜɪs ᴄᴏᴍᴍᴀɴғ 🥺")
+    helptext = "✪ 𝐇𝐞𝐥𝐩 𝐌𝐞𝐧𝐮 𝐎𝐟 ဂရပ်ထမယ် 𝐌𝐞𝐧𝐭𝐢𝐨𝐧\n\n✪ ᴄᴏᴍᴍᴀɴᴅ: /mentionall\n✪ ᴄᴏᴍᴍᴀɴᴅ: /cancel ᴛᴏ ᴄᴀɴᴄᴇʟ ɢᴏɪɴɢ ᴏɴ ᴘʀᴏᴄᴇss.\n✪ ᴄᴏᴍᴍᴀɴᴅ /admin ᴛᴏ ᴍᴇɴᴛɪᴏɴ ᴀʟʟ ᴀᴅᴍɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ\n✪ Yᴏᴜ ᴄᴀɴ ᴜsᴇ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ ᴡɪᴛʜ ᴛᴇxᴛ ᴡʜᴀᴛ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ᴍᴇɴᴛɪᴏɴ ᴏᴛʜᴇʀs.\n✪ `Example: /mentionall Good Morning!`\n✪ Yᴏᴜ ᴄᴀɴ ʏᴏᴜ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ ᴀs ᴀ ʀᴇᴘʟʏ ᴛᴏ ᴀɴʏ ᴍᴇssᴀɢᴇ. Bᴏᴛ ᴡɪʟʟ ᴛᴀɢ ᴜsᴇʀs ᴛᴏ ᴛʜᴀᴛ ʀᴇᴘʟɪᴇᴅ ᴍᴇsssᴀɢᴇ."
+    await event.reply(
+        helptext,
+        link_preview=False,
+        buttons=(
+            [
+                Button.url("☀︎︎ Group", "https://t.me/EwhWTBjxYuYxYTFl"),
+                Button.url("Channel ☀︎︎", "https://t.me/KoShine_Apaing"),
+            ]
+        ),
+    )
+
+
+@client.on(events.NewMessage(pattern="^/owner$"))
+async def help(event):
+    chat_id = event.chat_id
+    if not event.is_private:
+        return await event.respond("ᴅᴇᴀʀ sᴛᴀʀᴛ ᴍᴇ ɪɴ ᴘᴍ ᴛᴏ ᴜsᴇ ᴛʜɪs ᴄᴏᴍᴍᴀɴғ 🥺")
+    helptext = "✪ 𝐎𝐰𝐧𝐞𝐫 𝐌𝐞𝐧𝐮 𝐎𝐟 ဂရပ်ထမယ် 𝐌𝐞𝐧𝐭𝐢𝐨𝐧\n\n✪ ᴍʏ ᴏᴡɴᴇʀ ɪs [KAREN](https://t.me/KoShine_Apaing\n✪ The creator\n✪ Group(ttps://t.me/EwhWTBjxYuYxYTFl\n✪ "
+    await event.reply(
+        helptext,
+        link_preview=False,
+        buttons=(
+            [
+                Button.url("☀︎︎ Group ☀︎︎︎", "https://t.me/EwhWTBjxYuYxYTFl"),
+                Button.url("☀︎︎ Owner ☀︎︎", "https://t.me/KoShine_Apaing"),
+            ]
+        ),
+    )
+
+
+@client.on(events.NewMessage(pattern="^/mentionall|@mentionall|@all ?(.*)"))
 async def mentionall(event):
     chat_id = event.chat_id
     if event.is_private:
@@ -28,7 +88,7 @@ async def mentionall(event):
 
     is_admin = False
     try:
-        partici_ = await Client(GetParticipantRequest(event.chat_id, event.sender_id))
+        partici_ = await client(GetParticipantRequest(event.chat_id, event.sender_id))
     except UserNotParticipantError:
         is_admin = False
     else:
@@ -59,7 +119,7 @@ async def mentionall(event):
     spam_chats.append(chat_id)
     usrnum = 0
     usrtxt = ""
-    async for usr in Client.iter_participants(chat_id):
+    async for usr in client.iter_participants(chat_id):
         if not chat_id in spam_chats:
             break
         usrnum += 1
@@ -67,7 +127,7 @@ async def mentionall(event):
         if usrnum == 5:
             if mode == "text_on_cmd":
                 txt = f"{usrtxt}\n\n{msg}"
-                await Client.send_message(chat_id, txt)
+                await client.send_message(chat_id, txt)
             elif mode == "text_on_reply":
                 await msg.reply(usrtxt)
             await asyncio.sleep(2)
@@ -79,7 +139,7 @@ async def mentionall(event):
         pass
 
 
-@Client.on(events.NewMessage(pattern="^/admins|/admin|@admin|@admins ?(.*)"))
+@client.on(events.NewMessage(pattern="^/admins|/admin|@admin|@admins ?(.*)"))
 async def _(event):
     chat_id = event.chat_id
     if event.is_private:
@@ -87,7 +147,7 @@ async def _(event):
 
     is_admin = False
     try:
-        partici_ = await Client(GetParticipantRequest(event.chat_id, event.sender_id))
+        partici_ = await client(GetParticipantRequest(event.chat_id, event.sender_id))
     except UserNotParticipantError:
         is_admin = False
     else:
@@ -119,7 +179,7 @@ async def _(event):
     usrnum = 0
     usrtxt = ""
     chat = await event.get_input_chat()
-    async for x in Client.iter_participants(chat, filter=ChannelParticipantsAdmins):
+    async for x in client.iter_participants(chat, filter=ChannelParticipantsAdmins):
         if not chat_id in spam_chats:
             break
         usrnum += 1
@@ -127,7 +187,7 @@ async def _(event):
         if usrnum == 5:
             if mode == "text_on_cmd":
                 txt = f"{usrtxt}\n\n{msg}"
-                await Client.send_message(chat_id, txt)
+                await client.send_message(chat_id, txt)
             elif mode == "text_on_reply":
                 await msg.reply(usrtxt)
             await asyncio.sleep(2)
@@ -139,7 +199,7 @@ async def _(event):
         pass
 
 
-@Client.on(events.NewMessage(pattern="^/cancel$"))
+@client.on(events.NewMessage(pattern="^/cancel$"))
 async def cancel_spam(event):
     if not event.chat_id in spam_chats:
         return await event.respond("ᴛʜᴇʀᴇ ɪs ɴᴏ ᴘʀᴏᴄᴄᴇss ᴏɴ ɢᴏɪɴɢ...")
@@ -150,7 +210,7 @@ async def cancel_spam(event):
             pass
         return await event.respond("sᴛᴏᴘᴘᴇᴅ.")
 
-@Client.on(events.NewMessage(pattern="^/fools|ကျပ်မပြည့်တဲ့သုံးကောင်|ကျပ်မပြည့်တဲ့သုံးကောင်|@ကျပ်မပြည့်တဲ့သုံးကောင် ?(.*)")) 
+@client.on(events.NewMessage(pattern="^/fools|ကျပ်မပြည့်တဲ့သုံးကောင်|ကျပ်မပြည့်တဲ့သုံးကောင်|@ကျပ်မပြည့်တဲ့သုံးကောင် ?(.*)")) 
 async def help(event):
     chat_id = event.chat_id
     if not event.is_private:
@@ -160,7 +220,7 @@ async def help(event):
         helptext,
         link_preview=False )
 
-@Client.on(events.NewMessage(pattern="^/Mommy|@ယောက္ခမ ?(.*)")) 
+@client.on(events.NewMessage(pattern="^/Mommy|@ယောက္ခမ ?(.*)")) 
 async def help(event):
     chat_id = event.chat_id
     if not event.is_private:
