78aa0ba263009895487403c5b32e60c87f6a0a96
benjamin5250 <146529364+benjamin5250@users.noreply.github.com>
2023-11-03 22:31:01 +0630

    Update __init__.py

--- MukeshRobot/__init__.py
+++ MukeshRobot/__init__.py
@@ -161,7 +161,7 @@ DEV_USERS.add(abs(0b101100001110010100011000111101001))
 updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
 telethn = TelegramClient("mukesh", API_ID, API_HASH)
 
-pbot = Client("MukeshRobot", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN, ,in_memory=True)
+pbot = Client("MukeshRobot", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN, in_memory=True)
 app = Client(
     "MukeshRobot",
     mongodb=dict(connection=AsyncClient(DATABASE_URI), remove_peers=False),)
