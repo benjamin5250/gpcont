da3fa41fc0d10f628c23058c0d574620aefb7d5e
benjamin5250 <146529364+benjamin5250@users.noreply.github.com>
2023-11-03 21:59:58 +0630

    Update __init__.py

--- MukeshRobot/database/__init__.py
+++ MukeshRobot/database/__init__.py
@@ -1,5 +1,6 @@
 from typing import Dict, Union
 from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
+from async_pymongo import AsyncClient
 from MukeshRobot import MONGO_DB_URI
 
 mongo = MongoCli(MONGO_DB_URI)
